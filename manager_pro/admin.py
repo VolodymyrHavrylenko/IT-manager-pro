40672a6d764ca52806b8506c7e73e2bbaa6ab1b6
GavrilenkoVladimir <vladimirvgavrilenko@gmail.com>
2025-03-31 14:01:38 +0300

    Registered models in admin by class without implementation

--- manager_pro/admin.py
+++ manager_pro/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    Team,
+    Project,
+    Position,
+    TaskType,
+    Worker
+)
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Position)
+
+
+admin.site.register(TaskType)
